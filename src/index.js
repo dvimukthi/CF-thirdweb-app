f83d4e873c8e9211f5e66f10624efc52d84eba7f
dvimukthi <deshanvimukthi59@gmail.com>
2022-12-18 15:20:17 +0530

    import App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChaidId, ThirdwebProvider } from "@thirdweb-dev/react";
+import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
