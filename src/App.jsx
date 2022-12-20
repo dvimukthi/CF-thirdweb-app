c00ac2e0fe9f2ae65d9a0dab5db745d111353011
dvimukthi <deshanvimukthi59@gmail.com>
2022-12-20 23:04:49 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+    </div>
+  );
 };
 
 export default App;
