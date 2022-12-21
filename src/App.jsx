2302dc1e53f5b562ccc28830fb68e80901c45ce2
dvimukthi <deshanvimukthi59@gmail.com>
2022-12-21 23:54:59 +0530

    p

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+      <h1>CrowdFunding</h1>
+      <p className="font-xl font-bold">Hello Blockchain</p>
       <div className="flex-1 max-sw:w-full max-w-[1280px] mx-auto sm:pr-5">
         Navbar
       </div>
